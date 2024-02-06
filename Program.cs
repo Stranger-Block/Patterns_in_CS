f41601ec64513fd5dc0b762814dc30388c3c48e1
Stranger <blockchaindevloper2202@gmail.com>
2024-02-06 12:13:22 +0530

    new

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("1");
+Console.WriteLine("");
+
+for (int i = 0; i <= 5; i++)
+{
+    for (int j = i; j <= 5; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
+
+Console.WriteLine("");
+Console.WriteLine("2");
+Console.WriteLine("");
+
+for (int i = 0; i <= 5; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
