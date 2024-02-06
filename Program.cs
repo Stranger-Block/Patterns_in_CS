369e2302d365764f1be42d7e62cc7eb0919542d3
Stranger <blockchaindevloper2202@gmail.com>
2024-02-06 15:11:04 +0530

    Method's Code

--- Program.cs
+++ Program.cs
@@ -1,25 +1,130 @@
-﻿Console.WriteLine("Right Half Pyramid");
-Console.WriteLine("");
+﻿using System;
 
-for (int i = 0; i <= 5; i++)
+namespace Programm
 {
-    for (int j = 0; j <= i; j++)
+    class Code
     {
-        Console.Write("*");
-    }
+        public void RHP()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Inverted Right Half Pyramid");
+            Console.WriteLine("");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            } 
+        }
+
+        public void LHP()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Left Half Pyramid");
+            Console.WriteLine("");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write(" "); 
+                }
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        public void INHP()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Inverted Left Half Pyramid");
+            Console.WriteLine("");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" "); 
+                }
+
+                for (int k = i; k <= 5; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        public void IFP()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Inverted Full Pyramid");
+            Console.WriteLine("");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" "); 
+                }
+
+                for (int k = i; k <= 5; k++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        public void RP()
+{
     Console.WriteLine("");
+    Console.WriteLine("Rhombus Pattern");
+    Console.WriteLine("");
+
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int j = 0; j <= i; j++) 
+        {
+            Console.Write(" "); 
+        }
+
+        for (int k = 0; k <= 5; k++)
+        {
+            Console.Write("* ");
+        }
+
+        Console.WriteLine("");
+    }
+}
+        static void Main(string[] args)
+        {
+            Code m1 = new Code();
+
+            m1.RHP();
+        }
+
+    }
 }
 
-Console.WriteLine("");
-Console.WriteLine("Inverted Right Half Pyramid");
-Console.WriteLine("");
 
-for (int i = 0; i <= 5; i++)
-{
-    for (int j = i; j <= 5; j++)
-    {
-        Console.Write("*");
-    }
-    Console.WriteLine("");
-}
+
+
+
+
+
+
+
+
+
 
